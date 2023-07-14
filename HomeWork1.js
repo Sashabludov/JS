89b8216de0d612ba3bec9d8121cf59bd1cc63e9c
Sashabludov <136597743+Sashabludov@users.noreply.github.com>
2023-07-14 14:43:52 +0200

    Create HomeWork1.js

---
+++ HomeWork1.js
@@ -0,0 +1,36 @@
+var item_1 = 5
+console.log(item_1)
+var item_2 = 3
+console.log(item_2)
+var item_3 = item_1*item_2
+console.log(item_3)
+var item_4 = "yolochka"
+console.log(item_4)
+console.log(item_3, item_4)
+console.log(item_3 * item_4)
+var item_5 = item_3
+var item_6 = 15
+var item_6_type = item_6
+console.log("item_6 == ", item_6, "item_6_type == ", item_6_type)
+var item_7 = item_6
+item_7 = String(item_7)
+var item_7_type = item_7
+console.log("item_7 == ", item_7, "item_7_type == ", item_7_type)
+var age_1 = 10
+var age_2 = 18
+var age_3 = 60
+if (age_1){
+    console.log(age_1)
+}
+if (age_1 < age_2){
+    console.log("tou don't have acces cause your age is " + age_1 + " It's less then")
+
+}
+if (age_1 >= age_2 & age_1 < age_3){
+    console.log("Welcom  !")
+}
+if (age_1 > age_3){
+    console.log("Keep calm and look culture channel")
+} else{
+    console.log("Technical work")
+}
